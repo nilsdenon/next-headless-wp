f9cd3a4e6a25b7b1e768f3d2d3ea5bc4df77bd49
nils denon <nilsdenon@gmail.com>
2020-10-18 04:05:52 +0200

    Link

---
+++ components/Link.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+const ActiveLink = ({ children, ...props }) => {
+  const router = useRouter()
+  const child = React.Children.only(children)
+  return (
+    <Link {...props}>
+      {React.cloneElement(child, { active: router.pathname === props.href })}
+    </Link>
+  )
+}
+
+export default ActiveLink
